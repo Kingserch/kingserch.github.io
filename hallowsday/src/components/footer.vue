79c56010523aafef02c9ead66be84e425943c007
kingserch <838915764@qq.com>
2018-11-07 17:07:56 +0800

    1

---
+++ hallowsday/src/components/footer.vue
@@ -0,0 +1,44 @@
+<template>
+	<footer class="bottom"></footer>
+</template>
+<style scoped="" type="text/css">
+	.bottom{
+		width: 100%;
+        min-width: 1024px;
+		height: 120px;
+		background: url('https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-b01.png');
+		background-repeat-x: repeat;
+	    background-repeat-y: no-repeat;
+	    background-size: cover;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		z-index: 2;
+	}
+	.bottom:before{
+		content: '';
+	    width: 500px;
+	    display: inline-block;
+	    height: 400px;
+	    background: url(https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-b02.png);
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    position: absolute;
+	    left: 0;
+	    bottom: 30px;
+	    z-index: 3;
+	}
+	.bottom:after{
+		content: '';
+	    width: 700px;
+	    display: inline-block;
+	    height: 400px;
+         background: url(https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-b03.png);
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    position: absolute;
+	    right: 0;
+	    bottom: 30px;
+	    z-index: 3;
+	}
+</style>
